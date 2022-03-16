e4bbf78771d304016b28e0bf480104deb9a67def
LoncaricLaura <laura.loncaric032@gmail.com>
2022-03-16 23:41:14 +0100

    modified app.vue and home.vue, fetched api

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '/src/components/HelloWorld.vue'
+import Home from '/src/views/Home.vue'
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
     },
+
 ]
 const router = createRouter({
     history: createWebHistory(),
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+ 
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+    name: "Home",
+    data() {
+        return {
+            commits: [],
+        }
+    },
+    mounted() {
+        axios.get('https://api.github.com/repos/vuejs/vue/commits')
+        .then(response => {
+            this.commits = response.data.data;
+            console.log(response.data);
+        })
+    },
+}
+
+</script>
